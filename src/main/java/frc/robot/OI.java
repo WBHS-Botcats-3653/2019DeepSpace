01b41664b423d6cd4d9df26ed1c72c049fc98976
Alan Heyd <alanheyd@gmail.com>
2019-01-22 17:55:17 -0500

    Merged brook_drive_12Jan branch. Arcade drive working

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,36 +7,67 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID;
+
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
  */
+
 public class OI {
-  //// CREATING BUTTONS
-  // One type of button is a joystick button which is any button on a
-  //// joystick.
-  // You create one by telling it which joystick it's on and which button
-  // number it is.
-  // Joystick stick = new Joystick(port);
-  // Button button = new JoystickButton(stick, buttonNumber);
+	private static OI m_singleton = null;
+	private XboxController m_controller = null;
 
-  // There are a few additional built in buttons you can use. Additionally,
-  // by subclassing Button you can create custom triggers and bind those to
-  // commands the same as any other Button.
+	private OI()
+	{
+		m_controller = new XboxController( 0 );
+	}
 
-  //// TRIGGERING COMMANDS WITH BUTTONS
-  // Once you have a button, it's trivial to bind it to a button in one of
-  // three ways:
+	public static OI getInstance()
+	{
+		if(m_singleton ==  null)
+		{
+			m_singleton = new OI();
+		}
+		return m_singleton;
+	}
 
-  // Start the command when the button is pressed and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenPressed(new ExampleCommand());
+	public double getThrottle()
+	{
+		return -m_controller.getY(GenericHID.Hand.kLeft);
+	}
 
-  // Run the command while the button is being held down and interrupt it once
-  // the button is released.
-  // button.whileHeld(new ExampleCommand());
+	public double getSteering()
+	{
+		//its negative because the getX was inverted
+		return m_controller.getX(GenericHID.Hand.kRight)*0.5;
+	}
+	//// CREATING BUTTONS
+	// One type of button is a joystick button which is any button on a
+	//// joystick.
+	// You create one by telling it which joystick it's on and which button
+	// number it is.
+	// Joystick stick = new Joystick(port);
+	// Button button = new JoystickButton(stick, buttonNumber);
 
-  // Start the command when the button is released and let it run the command
-  // until it is finished as determined by it's isFinished method.
-  // button.whenReleased(new ExampleCommand());
+	// There are a few additional built in buttons you can use. Additionally,
+	// by subclassing Button you can create custom triggers and bind those to
+	// commands the same as any other Button.
+
+	//// TRIGGERING COMMANDS WITH BUTTONS
+	// Once you have a button, it's trivial to bind it to a button in one of
+	// three ways:
+
+	// Start the command when the button is pressed and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenPressed(new ExampleCommand());
+
+	// Run the command while the button is being held down and interrupt it once
+	// the button is released.
+	// button.whileHeld(new ExampleCommand());
+
+	// Start the command when the button is released and let it run the command
+	// until it is finished as determined by it's isFinished method.
+	// button.whenReleased(new ExampleCommand());
 }
---
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -0,0 +1,60 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+import frc.robot.RobotMap;
+import frc.robot.commands.ArcadeDriveCommand;
+/**
+ * Add your docs here.
+ */
+public class Drive extends Subsystem {
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	private static Drive m_singleton = null;
+	private WPI_TalonSRX m_leftDriveMaster, m_rightDriveMaster;
+	private WPI_VictorSPX m_leftDriveSlave, m_rightDriveSlave;
+	private DifferentialDrive m_drive;
+
+	@Override
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		setDefaultCommand(new ArcadeDriveCommand());
+	}
+
+	private Drive()
+	{
+		m_rightDriveMaster = new WPI_TalonSRX(RobotMap.canRightDriveMotorMaster);
+		m_leftDriveMaster = new WPI_TalonSRX(RobotMap.canLeftDriveMotorMaster);
+		m_rightDriveSlave = new WPI_VictorSPX(RobotMap.canRightDriveMotorSlave);
+		m_leftDriveSlave = new WPI_VictorSPX(RobotMap.canLeftDriveMotorSlave);
+
+		m_rightDriveSlave.follow(m_rightDriveMaster);
+		m_leftDriveSlave.follow(m_leftDriveMaster);
+
+		m_drive = new DifferentialDrive(m_leftDriveMaster, m_rightDriveMaster);
+	}
+
+	public void arcadeDrive(double xSpeed, double zRotation)
+	{
+		m_drive.arcadeDrive(xSpeed, zRotation);
+	}
+
+	public static Drive getInstance()
+	{
+		if (m_singleton == null)
+		{
+			m_singleton = new Drive();
+		}
+		return m_singleton;
+	}
+}
